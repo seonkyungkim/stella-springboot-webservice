834d5c74a375c960e8c598df46c438e514c8850d
stellakimm <ovvlvlbm@gmail.com>
2021-07-06 21:42:48 +0900

    Test cases added for post, update functions. It has a minor issue in PostsApiControllerTest.java line 55, 87.

---
+++ src/main/java/com/example/springboot/service/PostsService.java
@@ -0,0 +1,35 @@
+package com.example.springboot.service;
+
+import com.example.springboot.domain.posts.Posts;
+import com.example.springboot.domain.posts.PostsRepository;
+import com.example.springboot.web.dto.PostsResponseDto;
+import com.example.springboot.web.dto.PostsSaveRequestDto;
+import com.example.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("There is no such post. id= "+ id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("There is no such post. id= "+ id));
+
+        return new PostsResponseDto(entity);
+    }
+}
---
+++ src/main/java/com/example/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,22 @@
+package com.example.springboot.web.dto;
+
+import com.example.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    //Not all fields of Entity class are needed, Dto gets Entity as a parameter.
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+
+}
